69969a400d660cbc45cabd8c34eb726c97d1dae0
beckon111 <beckon.rus@gmail.com>
2018-03-31 01:35:34 +0300

    auth reg

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Http, RequestOptions } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -8,20 +11,43 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [
+      {
+          provide: AuthHttp,
+          useFactory: authHttpServiceFactory,
+          deps: [Http, RequestOptions]
+      }
+  ],
   bootstrap: [AppComponent]
-})
+});
+
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+    return new AuthHttp(new AuthConfig({
+        headerName: 'Authorization',
+        headerPrefix: '',
+        tokenName: 'token',
+        tokenGetter: (() => localStorage.getItem('id_token')),
+        globalHeaders: [{ 'Content-Type': 'application/json' }],
+        noJwtError: true
+    }), http, options);
+}
+
 export class AppModule { }
--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  ngOnInit(): void {
+  }
 
-  ngOnInit() {
+  email: string;
+  password: string;
+  error: string;
+
+  constructor(private router: Router) {
+  }
+
+  login(): void {
   }
 
 }
