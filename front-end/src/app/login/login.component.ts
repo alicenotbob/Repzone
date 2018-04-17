11b2e91e71245b6f4cb7cdbae7451946d1819b18
vtarasevich <v2.tarasevich@itransition.com>
2018-04-17 22:44:49 +0300

    refactoring

--- front-end/src/app/login/login.component.ts
+++ front-end/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from "../service/auth.service";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -9,25 +10,34 @@ import {AuthService} from "../service/auth.service";
 })
 export class LoginComponent implements OnInit {
 
+  loginForm: FormGroup;
   email: string;
   password: string;
   error: string;
 
-  constructor(private router: Router,
+  constructor(private fb: FormBuilder,
+              private router: Router,
               private authService: AuthService) {
   }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
   login() {
-    this.authService.login(this.email, this.password).subscribe(
-      data => {
-        this.router.navigate(['']);
-      },
-      err => {
-        this.error = 'Invalid email or password';
-      }
-    );
+    const credentials = this.loginForm.value;
+    if(credentials.email && credentials.password) {
+      this.authService.login(credentials.email, credentials.password).subscribe(
+        data => {
+          this.router.navigate(['/']);
+        },
+        err => {
+          this.error = 'Invalid email or password';
+        }
+      );
+    }
   }
 }
